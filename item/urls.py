8da93e05d375475af0cb4ef509b9dfeca442151d
Maisha Motlib-Siddiqui <mmotlibs@amazon.co.uk>
2023-08-04 10:54:28 +0100

    [VM-6] Implemented user login functionality.

--- item/urls.py
+++ item/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     # Main page displaying all items.
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # User login page.
+    path('login/', views.log, name='log'),
+
+    # Endpoint to authenticate user login details.
+    path('login/authenticate/', views.authenticateView, name='authenticate'),
 ]
--- item/views.py
+++ item/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.db.models import Sum
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Item
 
@@ -9,3 +13,22 @@ def index(request):
     item_list = Item.objects.all().values('id', 'itemType', 'itemPrice').annotate(
         itemQuantity=Sum('itemQuantity'))
     return render(request, 'index.html')
+
+# Renders the login page. Displays an error message if login fails.
+def log(request, loginFailed=False):
+    return render(request, 'login.html', {'loginFailed': loginFailed})
+
+# Authenticates user credentials and logs them in if valid.
+def authenticateView(request):
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        # If user is authenticated, log them in and redirect to index.
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+
+        # If authentication fails, render the login page with an error message.
+        return render(request, 'login.html', {'loginFailed': True})
