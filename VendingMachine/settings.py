b7eda7322e81be8cdc09236a8f95939f265da263
Maisha Motlib-Siddiqui <mmotlibs@amazon.com>
2024-03-04 16:41:07 +0000

    Turning off prod debugging

--- VendingMachine/settings.py
+++ VendingMachine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Use HTTPS for cookies
 CSRF_COOKIE_SECURE = True
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
+    'storages'
 ]
 
 if os.environ.get('TESTING') == 'TRUE':
